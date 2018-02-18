3c39148ab5403d481f3554cbe96972c4eeb42d1d
Julien Delarocque <jkd1112@gmail.com>
2018-02-18 19:01:45 +0100

    new content, adapted stylesheet for lists and dropdown, added google verification

--- _includes/script.js
+++ _includes/script.js
@@ -6,13 +6,13 @@ function check() {
     if (insulin != 0) {
       cutoff=Math.round(9.515353+2.623057*time-0.04145439*time*time+0.0003201212*time*time*time-0.0000008373978*time*time*time*time);
       if (insulin<=0.9*cutoff) {
-        document.getElementById("result").innerHTML = "<strong>This value seems fine.</strong> Please remmember that the proposed reference can only be considered valid when the protocol described in the paper this Webapp is based upon was followed and insulin was measured with Mercodia Equine Insulin ELISA.";
+        document.getElementById("result").innerHTML = "<strong>The insulin response to the standard dosed OGT is considered normal.</strong> Please remember that the proposed reference can only be considered valid when the protocol described in the paper this Webapp is based upon was followed and insulin was measured with Mercodia<sup>&reg;</sup> Equine Insulin ELISA.";
         document.getElementById("result").style.backgroundColor = "#d8f2d2";
       } else if (insulin>1.1*cutoff) {
-        document.getElementById("result").innerHTML = "This level of insulin at the given time-point of the OGT is considered to be <strong>indicative of insulin dysregulation</strong>. An optimisation of the horse's managment is higly recommended.";
+        document.getElementById("result").innerHTML = "This level of insulin at the given time-point of the standard dosed OGT is considered to be <strong>indicative of insulin dysregulation</strong>. An optimization of the horse's management is highly recommended. See <a href='#suggested'>suggested reading</a>.";
         document.getElementById("result").style.backgroundColor = "#f7bbb9";
       } else {
-        document.getElementById("result").innerHTML = "A reliable diagnosis can not be provided upon this value. It is considered to be in a <strong>grey zone</strong>. We suggest retesting the patient in a few weeks. As references are available for the range 90 to 180 min, it is of course possible to take more than one sample to augment the sensitivity of the OGT. This is especially recommended when insulin status is unclear.";
+        document.getElementById("result").innerHTML = "A reliable diagnosis cannot be provided upon this value. It is considered to be in a <strong>grey zone</strong>. We suggest retesting the patient in a few weeks. As references are available for the range 90 to 180 min, it is of course possible to take more than one sample to augment the sensitivity of the OGT. This is especially recommended when insulin status is unclear.";
         document.getElementById("result").style.backgroundColor = "#f7e9b9";
       }
     }
